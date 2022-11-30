6c1f7f7f7499cacf97027047b2ea9e370d048d14
GabrielMeira <gabegm03@outlook.com>
2022-11-30 02:48:41 -0300

    Ajuste envio de e-mail 2.0

--- server/src/adapters/nodemailer/NodemailerMailAdapter.ts
+++ server/src/adapters/nodemailer/NodemailerMailAdapter.ts
@@ -1,19 +1,17 @@
 import { MailAdapter, sendMailData } from "../MailAdapter";
 import nodemailer from "nodemailer";
 
-const transport = nodemailer.createTransport({
+var transport = nodemailer.createTransport({
     host: "smtp.mailtrap.io",
     port: 2525,
     auth: {
-        user: "49303e7e128341",
-        pass: "ab722e9acf3e77"
+      user: "7b351ad1a40dae",
+      pass: "1b95b7a716d99a"
     }
-})
+  });
 
 
 export class NodemailerMailAdapter implements MailAdapter {
-
-
     async sendMail({subject, body}: sendMailData) {
 
         await transport.sendMail({
